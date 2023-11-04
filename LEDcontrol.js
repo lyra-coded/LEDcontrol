de1b73802f185f8151b267eb3cab023cd475a0e7
chris <wsmeorc.b2@gmail.com>
2023-11-04 01:02:20 -0400

    Added possibly janky error handling for serial port not being found

--- LEDcontrol.js
+++ LEDcontrol.js
@@ -1,25 +1,31 @@
 // Tutorial used: https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=5
-
-var http = require('http');
-const { express } = require('express')
+const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname +_'/views')
+app.set('views', (__dirname + 'views'))
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
 var readData = "";
 
-// listen for serail data
+// listen for serial data
 const port = new SerialPort({
     path: '/dev/ttyACM0',
     baudRate: 9600
 })
+
+// Handle any errors that occur when opening the serial port
+port.on('error', (err) => {
+    console.error('Serial port error:', err.message);
+});
+
+// Read data from serial device
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 parser.on('data', (serialdata) => {
     console.log(serialdata);
@@ -27,7 +33,4 @@ parser.on('data', (serialdata) => {
 });
 
 // run server
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(readData);
-}).listen(4000);
+app.listen(process.env.PORT || 3000)
