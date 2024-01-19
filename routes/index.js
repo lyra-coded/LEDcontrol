434f70f714bd0c88676f47ffe3d8076824c91ba8
chris <wsmeorc.b2@gmail.com>
2024-01-18 23:12:10 -0500

    added stylesheets

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const AColorPicker = require('a-color-picker')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index')
+
 })
 
 router.post('/', (req, res) => {
+    AColorPicker.from('.picker')
+    .on('change', (picker, color) => {
+    document.body.style.backgroundColor = color;
+    });
     res.send('value')
 })
 
