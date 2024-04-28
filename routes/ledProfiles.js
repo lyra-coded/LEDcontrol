d4877a38174e455eadd83ff18d93d7e6cc73d19e
chris <wsmeorc.b2@gmail.com>
2024-04-28 10:16:29 -0400

    color picker is now displayed on frontend

--- routes/ledProfiles.js
+++ routes/ledProfiles.js
@@ -1,25 +1,13 @@
 const AColorPicker = require('a-color-picker')
 const express = require('express')
+const { SerialPort } = require('serialport')
 const router = express.Router()
 
-router.get('/ledProfiles', (req, res) => {
+router.get('/', (req, res) => {
     res.render('ledProfiles/index')
-
-    AColorPicker.from('.picker')
-    .on('change', (picker, color) => {
-
-    })
-    .on('coloradd', (picker, color) => {
-      // color added: color
-      // modified palette: picker.palette
-    })
-    .on('colorremove', (picker, color) => {
-      // color removed: color
-      // modified palette: picker.palette
-    });
 })
 
-router.post('/ledProfiles', (req, res) => {
+router.post('/', (req, res) => {
     res.send('value')
 })
 
